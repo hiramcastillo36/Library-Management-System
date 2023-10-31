45e755e22647bd55005dbac6f3e0dde5dd7f73a2
Eloy Hernández <eloyhz@gmail.com>
2023-10-31 12:40:53 -0600

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module library.library {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens library.library to javafx.fxml;
+    exports library.library;
+}
