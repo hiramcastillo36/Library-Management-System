03860ecd48114a32f83e181fc022f5b506a5a781
katahanz1 <andr3s.aguirr3.o@gmail.com>
2023-11-25 11:59:54 -0600

    Connection between AccountInterface and Interface

--- src/main/java/library/library/AccountInterface/AccountInterface.java
+++ src/main/java/library/library/AccountInterface/AccountInterface.java
@@ -1,8 +1,14 @@
 package library.library.AccountInterface;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import library.library.LibraryApplication;
+
+import java.io.IOException;
 
 public class AccountInterface {
 
@@ -11,7 +17,17 @@ public class AccountInterface {
 
     @FXML
     void goBack(MouseEvent event) {
+        try {
+            // Cargar la nueva escena (en este caso, la escena anterior)
+            FXMLLoader loader = new FXMLLoader(LibraryApplication.class.getResource("view/Interface.fxml"));
+            Scene previousScene = new Scene(loader.load());
 
+            // Obtener el Stage actual y cambiar su escena
+            Stage currentStage = (Stage) back.getScene().getWindow();
+            currentStage.setScene(previousScene);
+        } catch (IOException e) {
+            e.printStackTrace(); // Manejo básico de excepciones, ajusta según sea necesario
+        }
     }
 
 }
